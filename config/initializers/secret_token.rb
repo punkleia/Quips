9f43b80e03c11e7fe7c28136702f31f128106d75
Michelle Luby <theatrejawa@yahoo.com>
2017-01-05 03:44:11 +0000

    New initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quips::Application.config.secret_key_base = '66a41f29ed2161045a77879e3cad8e8878e56c47c3cd5e47565757bbac0a86191830dab9acf4ef47ec3f56c93e03150b3b219a48c7f4afbdbe5b3fbd935246ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Quips::Application.config.session_store :cookie_store, key: '_quips_session'
